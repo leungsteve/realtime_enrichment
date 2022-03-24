877bfbbd12b33b1b2c08dc6ded33fcb28d92b508
leungsteve <leungsteve@users.noreply.github.com>
2022-03-24 19:59:38 +0000

    deploy: 0d15b396ae02c59389b97f3d874c62d4d819b1f6

--- flask_apps/sentiment/start_sentiment.sh
+++
@@ -1,7 +0,0 @@
-pip install 'splunk-opentelemetry[all]'
-splunk-py-trace-bootstrap
-export OTEL_SERVICE_NAME='sentiment'
-export OTEL_EXPORTER_OTLP_ENDPOINT='http://myotel:4317'
-export OTEL_RESOURCE_ATTRIBUTES='deployment.environment=lab'
-splunk-py-trace python sentiment.py
-
--- flask_apps/user_lookup/start_user_lookup.sh
+++
@@ -1,8 +0,0 @@
-deactivate
-source venv/bin/activate
-splunk-py-trace-bootstrap
-export OTEL_SERVICE_NAME='user-lookup'
-export OTEL_EXPORTER_OTLP_ENDPOINT='http://myotel:4317'
-export OTEL_RESOURCE_ATTRIBUTES='deployment.environment=lab'
-splunk-py-trace python user_lookup.py
-
